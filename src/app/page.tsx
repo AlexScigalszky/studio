e513706cb91672a8e6f7e7d876f7dc22eb3b417b
Alex Scigalszky <alexpablo.90@gmail.com>
2025-04-17 18:18:37 +0000

    make 30% for sidebar and the rest for preview

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,7 +29,7 @@ export default function Home() {
   return (
     <SidebarProvider>
       <div className="flex h-screen overflow-hidden">
-        <Sidebar className="w-80 border-r flex-shrink-0">
+        <Sidebar className="w-1/3 border-r flex-shrink-0">
           <SidebarHeader>
             <h2>Frame Configuration</h2>
           </SidebarHeader>
@@ -46,7 +46,7 @@ export default function Home() {
             />
           </SidebarContent>
         </Sidebar>
-        <div className="flex-1 p-4">
+        <div className="flex-1 p-4 w-2/3">
           <VisualPlacementPreview
             frameDimensions={frameDimensions}
             wallDimensions={wallDimensions}
